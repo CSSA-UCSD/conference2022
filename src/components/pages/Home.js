7f7fa9595e7b279b67e98f2545266aded964fbf8
Manda Tran <mandatran3@gmail.com>
2021-02-14 09:59:25 -0800

    design changes

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -25,30 +25,50 @@ class Home extends React.Component {
         const ScheduleComponent = DayScheduleData.map(item => < DaySchedule key={item.id} item={item}/>);
         const SpeakerComponent = SpeakerData.map(item => < SpeakerPreview key={item.id} item={item}/>);
         const responsive = {
-            superLargeDesktop: {
-              // the naming can be any, depends on you.
-              breakpoint: { max: 4000, min: 3000 },
-              items: 5
-            },
             desktop: {
-              breakpoint: { max: 3000, min: 1024 },
-              items: 3
-            },
-            tablet: {
-              breakpoint: { max: 1024, min: 464 },
-              items: 2
-            },
-            mobile: {
-              breakpoint: { max: 464, min: 0 },
-              items: 1
-            }
+                breakpoint: { max: 3000, min: 1024 },
+                items: 2,
+                slidesToSlide: 2 // optional, default to 1.
+              },
+              tablet: {
+                breakpoint: { max: 1024, min: 464 },
+                items: 2,
+                slidesToSlide: 2 // optional, default to 1.
+              },
+              mobile: {
+                breakpoint: { max: 464, min: 0 },
+                items: 1,
+                slidesToSlide: 1 // optional, default to 1.
+              }
           };
 
         return (
-            <div className="container">
+            <div className="home container">
                 <div className="row py-5">
                     <div className="home-img col-sm-6">
-                        <img src={HomeImg} alt="Home Graphic"></img>
+                        <svg width="447" height="447" viewBox="0 0 447 447" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M341.243 365.407C338.235 370.305 334.931 378.89 344.594 383.467C356.558 389.134 369.152 358.909 376.507 345.887" stroke="white" stroke-width="8.10409" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M323.223 252.362C257.06 358.35 270.793 378.506 313.697 316.361C365.709 241.024 332.002 317.297 332.002 317.297" stroke="white" stroke-width="8.10409" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M325.158 328.55C325.158 328.55 280.528 413.207 333.331 342.964C399.335 255.16 349.582 347.931 345.651 355.284" stroke="white" stroke-width="8.10409" stroke-linecap="round" stroke-linejoin="round"/>
+                            <circle cx="341.133" cy="363.582" r="2.6716" transform="rotate(-45 341.133 363.582)" fill="white"/>
+                            <circle cx="375.811" cy="346.231" r="10.9011" transform="rotate(-45 375.811 346.231)" fill="white"/>
+                            <circle cx="323.137" cy="330.814" r="8.17581" transform="rotate(-45 323.137 330.814)" fill="white"/>
+                            <path d="M311.019 232.449C311.019 232.449 294.988 222.793 280.02 158.73C280.02 158.73 266.55 114.576 251.169 96.4007C231.749 73.4524 211.719 60.8682 194.013 54.1665" stroke="white" stroke-width="10.8055" stroke-linecap="round" stroke-linejoin="round"/>
+                            <circle cx="325.794" cy="247.222" r="10.9011" transform="rotate(-45 325.794 247.222)" fill="white"/>
+                            <path d="M49.2226 137.874C49.2226 137.874 32.5847 196.788 96.6032 250.965C114.778 266.346 158.932 279.816 158.932 279.816C222.995 294.784 247.325 322.001 247.325 322.001L255.593 330.269" stroke="white" stroke-width="10.8055" stroke-linecap="round" stroke-linejoin="round"/>
+                            <circle cx="256.415" cy="332.011" r="10.9011" transform="rotate(-45 256.415 332.011)" fill="white"/>
+                            <path d="M57.3662 112.403C57.3662 112.403 69.4422 75.3594 105.114 58.9518C140.786 42.5441 169.058 49.3756 169.058 49.3756" stroke="white" stroke-width="10.8055" stroke-linecap="round" stroke-linejoin="round"/>
+                            <circle cx="169.061" cy="49.3763" r="10.9011" transform="rotate(-45 169.061 49.3763)" fill="white"/>
+                            <path d="M124.093 176.558C126.931 179.396 126.931 183.998 124.093 186.836C121.255 189.674 116.653 189.674 113.815 186.836C110.977 183.998 110.977 179.396 113.815 176.558C116.653 173.72 121.255 173.72 124.093 176.558Z" fill="white"/>
+                            <circle cx="158.781" cy="180.413" r="7.26739" transform="rotate(-45 158.781 180.413)" fill="white"/>
+                            <circle cx="165.207" cy="202.255" r="7.26739" transform="rotate(-45 165.207 202.255)" fill="white"/>
+                            <circle cx="138.484" cy="141.633" r="7.26739" transform="rotate(-45 138.484 141.633)" fill="white"/>
+                            <circle cx="170.344" cy="132.88" r="7.26739" transform="rotate(-45 170.344 132.88)" fill="white"/>
+                            <circle cx="197.323" cy="118.748" r="7.26739" transform="rotate(-45 197.323 118.748)" fill="white"/>
+                            <path d="M234.286 217.813L234.267 184.748L215.482 165.963L203.153 165.956L172.311 135.115M234.286 217.813L239.333 222.86M234.286 217.813L225.008 217.808M239.333 222.86L262.984 246.511L300.607 263.411M239.333 222.86L202.905 222.839M202.905 222.839L193.933 213.867L171.783 191.717L163.372 183.306M202.905 222.839L184.971 222.828L168.429 206.286M198.608 120.032L225.004 146.428L225.008 164.287L225.008 217.808M225.008 217.808L214.671 217.802L193.082 196.213L193.071 176.038L170.08 153.047L152.148 155.278L139.104 142.234" stroke="white" stroke-width="5.40273" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M119.769 182.445L131.098 193.774L131.112 216.978L145.937 231.804L162.152 231.813M162.152 231.813L230.366 231.853L254.194 255.681L264.906 285.531M162.152 231.813L142.571 212.232M275.617 315.382L264.906 285.531M264.906 285.531L274.406 263.92" stroke="white" stroke-width="5.40273" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+
                     </div>
                     <div className="home-title col-sm-6 ">
                         <h2>The emerging applications of cognitive science in technology, healthcare, and current global issues.</h2>
