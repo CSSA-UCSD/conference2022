55d3d6cb549de68460585e8cb84efaa8f21f172c
Manda Tran <mandatran3@gmail.com>
2021-02-28 12:39:09 -0800

    work on speakers and popup

--- src/components/components/home/SpeakerPreview.js
+++ src/components/components/home/SpeakerPreview.js
@@ -5,14 +5,16 @@ class SpeakerPreview extends React.Component {
     render() {
         return (
             <div className="carousel-speaker mx-2">
-                <div className="speaker-img pb-3">
-                    <img src={ require(`../../../img/speakers/${this.props.item.img}`).default } 
-                        alt={this.props.item.name} ></img>
-                </div>
-                <div className="speaker-title">
-                    <h5>{this.props.item.name}</h5>
-                    <p>{this.props.item.title}</p>
-                </div>
+                <a href={`/#speakers#` + this.props.item.id}>
+                    <div className="speaker-img pb-3">
+                        <img src={ require(`../../../img/speakers/${this.props.item.img}`).default } 
+                            alt={this.props.item.name} ></img>
+                    </div>
+                    <div className="speaker-title">
+                        <h5>{this.props.item.name}</h5>
+                        <p>{this.props.item.title}</p>
+                    </div>
+                </a>
             </div>
         );
     }
--- src/components/components/speakers/Speaker.js
+++ src/components/components/speakers/Speaker.js
@@ -1,6 +1,8 @@
 import React from "react";
 import AOS from "aos";
 
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 class Speaker extends React.Component {
 
@@ -11,18 +13,34 @@ class Speaker extends React.Component {
     }
 
     render() {
-
-
         return (
-            <div className="speaker col-md-4 py-3" data-aos="fade-up">
+            <div className="speaker col-md-4 py-3" data-aos="fade-up" id={this.props.item.id}>
                 <div className="speaker-img pb-3">
                     <img src={ require(`../../../img/speakers/${this.props.item.img}`).default } 
                         alt={this.props.item.name} ></img>
                 </div>
                 <div className="speaker-title">
                     <h5>{this.props.item.name}</h5>
-                    <p>{this.props.item.title}</p>
+                    {/* <p>{this.props.item.title}</p> */}
+                    <p>{this.props.item.event}</p>
                 </div>
+                <Popup
+                    trigger={<button className="button"> Open Modal </button>}
+                    modal
+                    nested
+                >
+                    {close => (
+                    <div className="modal">
+                        <button className="close" onClick={close}>
+                        &times;
+                        </button>
+                        <div className="header"> <h5>{this.props.item.name}</h5></div>
+                        <div className="content">
+                            <p>{this.props.item.bio}</p>
+                        </div>
+                    </div>
+                    )}
+                </Popup> 
             </div>
         );
     }
