ea08a47b29027135e227c4287f50b59c2e510e3e
Manda Tran <mandatran3@gmail.com>
2021-03-01 10:40:11 -0800

    test new link

--- src/components/components/home/SpeakerPreview.js
+++ src/components/components/home/SpeakerPreview.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class SpeakerPreview extends React.Component {
 
     render() {
         return (
             <div className="carousel-speaker mx-2">
-                <a href={`conference2021/#/speakers/#` + this.props.item.id}>
+                <Link to={'/speakers/#' + this.props.item.id} className='nav-link'>
+                {/*
+                <a href={`#/speakers/#` + this.props.item.id}>
+                */}
                     <div className="speaker-img pb-3">
                         <img src={ require(`../../../img/speakers/${this.props.item.img}`).default } 
                             alt={this.props.item.name} ></img>
@@ -14,7 +18,10 @@ class SpeakerPreview extends React.Component {
                         <h5>{this.props.item.name}</h5>
                         <p>{this.props.item.title}</p>
                     </div>
+                {/*
                 </a>
+                */}
+                </Link>
             </div>
         );
     }
