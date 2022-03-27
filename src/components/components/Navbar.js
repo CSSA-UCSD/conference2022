ddc7c356c2af489100222c7b5004666a89e41018
Vicky <v2chou@ucsd.edu>
2022-03-27 01:00:06 -0700

    updated fonts and logo

--- src/components/components/Navbar.js
+++ src/components/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Logo from "../../img/logos/Wordmark_Dark.svg";
+// import Logo from "../../img/logos/Wordmark_Dark.svg";
+import Logo from "../../assets/Logo with glow.png";
+import LogoNoGlow from "../../assets/Logo without glow.png";
 
 class Navbar extends React.Component {
     render() {
