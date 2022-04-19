450196691534aa8dc610ac611f628cac909c7a6a
Vicky <v2chou@ucsd.edu>
2022-04-19 16:07:40 -0700

    removed commmented events 2 in DayScheduleData

--- src/components/data/home/DayScheduleData.js
+++ src/components/data/home/DayScheduleData.js
@@ -31,9 +31,6 @@ const DayScheduleData = [
         date: "WED 4/27/2022 | 5-7:10 PM PST",
         img: "winter2021/IntroToDataAnalytics.png",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        // event1: "Keynote Speaker: TBA",  
-        // event2: "Social/Break",  
-        // event3: "Workshops: Design, Cognitive Behavioral Neuroscience, Clinical Aspects of Cognition",  
         events: [
             {
                 title: "Keynote Speaker: Vasudev Lal [Intel]",
