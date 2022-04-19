dae6721478917aa70811e7af3b879909a2755742
Vicky <v2chou@ucsd.edu>
2022-04-19 16:06:11 -0700

    updated day schedule on home

--- src/components/data/home/DayScheduleData.js
+++ src/components/data/home/DayScheduleData.js
@@ -5,9 +5,6 @@ const DayScheduleData = [
         date: "TUES 4/26/2022 | 5-7:10 PM PST",
         img: "winter2021/IntroToDataAnalytics.png",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        // event1: "Keynote Speaker: TBA", 
-        // event2: "Social/Break",  
-        // event3: "Workshops: Machine Learning, Neuroscience, Language/Culture",  
         events: [
             {
                 title: "Welcome Introduction",
@@ -34,9 +31,23 @@ const DayScheduleData = [
         date: "WED 4/27/2022 | 5-7:10 PM PST",
         img: "winter2021/IntroToDataAnalytics.png",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        event1: "Keynote Speaker: TBA",  
-        event2: "Social/Break",  
-        event3: "Workshops: Design, Cognitive Behavioral Neuroscience, Clinical Aspects of Cognition",  
+        // event1: "Keynote Speaker: TBA",  
+        // event2: "Social/Break",  
+        // event3: "Workshops: Design, Cognitive Behavioral Neuroscience, Clinical Aspects of Cognition",  
+        events: [
+            {
+                title: "Keynote Speaker: Vasudev Lal [Intel]",
+                time: "5-5:50pm PDT"
+            },
+            {
+                title: "Social Activity/Break",
+                time: "5:50-6:20pm PDT"
+            },
+            {
+                title: "Workshop: Neuroscience w/ Stephanie Alfonso",
+                time: "6:20-7:10pm PDT"
+            }
+        ],
         url: "",
     },
     {
@@ -45,9 +56,23 @@ const DayScheduleData = [
         date: "THURS 4/28/2022 | 5-7:10 PM PST",
         img: "winter2021/IntroToDataAnalytics.png",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        event1: "Career Panel",  
-        event2: "Social/Break",  
-        event3: "Poster Session",  
+        // event1: "Career Panel",  
+        // event2: "Social/Break",  
+        // event3: "Poster Session",  
+        events: [
+            {
+                title: "Career Panel",
+                time: "5-5:50pm PDT"
+            }, 
+            {
+                title: "Social Activity/Break",
+                time: "5:50-6:20pm PDT"
+            },
+            {
+                title: "Research/Lab Symposium: Virginia De Sa, Resarch/Lab Symposium: Prof. Wiggins (SDSU)",
+                time: "6:20-7:10pm PDT"
+            }
+        ],
         url: "",
     },
 ]
