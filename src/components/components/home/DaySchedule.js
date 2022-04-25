e4804cc9f789687c1d55b317a2adda61082f212a
Vicky <v2chou@ucsd.edu>
2022-04-25 13:50:02 -0700

    removed old code comments in day schedule

--- src/components/components/home/DaySchedule.js
+++ src/components/components/home/DaySchedule.js
@@ -17,30 +17,15 @@ class DaySchedule extends React.Component {
             <div className="day-schedule pb-5" data-aos="fade-up">
                 <div className="day-schedule-content">
                     <div className="p-4 row container">
-                        {/*
-                        <div className="day-img col-4">
-                            <img src={Testing} alt="test"></img> 
-                        </div>
-                        */}
-                        {/* <div className="day-info col-8"> */}
                         <div className="day-info">
                             <h4>{this.props.item.date}</h4>
                             <h2>{this.props.item.name}</h2>
                             {/* 
                             <p>{this.props.item.description}</p>
                             */}
-                            {/* {
-                                this.props.item.event1 && 
-                                this.props.item.evenTime1 && 
-                                <p><span>{this.props.item.eventTime1}</span> {this.props.item.event1}</p>
-                            } */}
-                            {/* {console.log(this.props.item.events)} */}
                             { this.props.item.events && this.props.item.events.map(event => {
-                                // this.props.items.eventTime[index]
                                 return <p><span>{event.time}</span> {event.title}</p>
                             })}
-                            {/* <p><span>5:50-6:20 PM PST </span>{this.props.item.event2}</p> */}
-                            {/* <p><span>6:20-7:10 PM PST </span>{this.props.item.event3}</p> */}
                         </div>
                     </div>
                 </div>
