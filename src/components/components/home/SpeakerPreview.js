a47213ce6e825690b8948dcbc23c82c619bf9e30
Vicky <v2chou@ucsd.edu>
2022-04-25 16:22:32 -0700

    edited speaker title css

--- src/components/components/home/SpeakerPreview.js
+++ src/components/components/home/SpeakerPreview.js
@@ -12,16 +12,14 @@ class SpeakerPreview extends React.Component {
                 {/* <Link to={'/speakers/#' + this.props.item.id} className='nav-link'> */}
                 <a href={`/speakers/#` + this.props.item.id}>
                
-                    <div className="speaker-img pb-3">
-                        <img src={require(`../../../img/speakers/${this.props.item.img}`).default}
+                    <div className="pb-3">
+                        <img className="speaker-img" src={require(`../../../img/speakers/${this.props.item.img}`).default}
                             alt={this.props.item.name} ></img>
                         <div className="speaker-title">
                             <h5>{this.props.item.name}</h5>
                             <p>{this.props.item.title}</p>
                         </div>
-
-                    </div>
-                    
+                    </div>                    
                 </a>
                
                 </Link>
